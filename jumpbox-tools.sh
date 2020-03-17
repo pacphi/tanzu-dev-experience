d0498ed26cb27f4a0027c75fe3b09e6438f81029
Chris Phillipson <fastnsilver@gmail.com>
2020-03-17 07:53:14 -0700

    Script improvements

--- jumpbox-tools.sh
+++ jumpbox-tools.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
+cd ~
+
 # Install prerequisites
 
 sudo apt update --yes && \
 sudo apt install --yes build-essential curl default-jdk jq git python-pip python-dev wget && \
 sudo pip install --upgrade pip
 
+# This should be the value of Legacy API Token, found here: https://network.pivotal.io/users/dashboard/edit-profile
 PIVNET_UAA_REFRESH_TOKEN=change_me
 
-cd ~
+AZ_REPO=$(lsb_release -cs)
+BOSH_VERSION=6.2.1
+CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
+LEFTOVERS_VERSION=0.62.0
+PIVNET_VERSION=1.0.1
+TF_VERSION=0.12.23
 
 pip install awscli
 
-export AZ_REPO=$(lsb_release -cs) && \
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | \
 sudo tee /etc/apt/sources.list.d/azure-cli.list && \
 sudo apt-key --keyring /etc/apt/trusted.gpg.d/Microsoft.gpg adv \
@@ -21,7 +28,6 @@ sudo apt-key --keyring /etc/apt/trusted.gpg.d/Microsoft.gpg adv \
 sudo apt update && \
 sudo apt install --yes azure-cli
 
-BOSH_VERSION=6.2.1
 wget -O bosh https://s3.amazonaws.com/bosh-cli-artifacts/bosh-cli-${BOSH_VERSION}-linux-amd64 && \
 chmod +x bosh && \
 sudo mv bosh /usr/local/bin/
@@ -31,19 +37,16 @@ sudo systemctl start docker && \
 sudo systemctl enable docker && \
 sudo usermod -aG docker ${USER}
 
-export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)" && \
 echo "deb http://packages.cloud.google.com/apt ${CLOUD_SDK_REPO} main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list && \
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add - && \
 apt update -y && apt install google-cloud-sdk -y
 
 curl -L https://k14s.io/install.sh | sudo bash
 
-PIVNET_VERSION=1.0.1
 wget -O pivnet https://github.com/pivotal-cf/pivnet-cli/releases/download/v${PIVNET_VERSION}/pivnet-linux-amd64-${PIVNET_VERSION} && \
 chmod +x pivnet && \
 sudo mv pivnet /usr/local/bin/
 
-TF_VERSION=0.12.23
 wget -O terraform.zip https://releases.hashicorp.com/terraform/${TF_VERSION}/terraform_${TF_VERSION}_linux_amd64.zip && \
 unzip terraform.zip && \
 sudo mv terraform /usr/local/bin && \
@@ -59,7 +62,6 @@ curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s http
 chmod +x kubectl && \
 sudo mv kubectl /usr/local/bin
 
-LEFTOVERS_VERSION=0.62.0
 wget https://github.com/genevieve/leftovers/releases/download/v${LEFTOVERS_VERSION}/leftovers-v${LEFTOVERS_VERSION}-linux-amd64 && \
 mv leftovers-v${LEFTOVERS_VERSION}-linux-amd64 leftovers && \
 chmod +x leftovers && \
