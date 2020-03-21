b3b71c8c16f3fc14439a9239799374ed9beaa89e
Chris Phillipson <fastnsilver@gmail.com>
2020-03-21 11:32:21 -0700

    Optimize install tools script

--- jumpbox-tools.sh
+++ jumpbox-tools.sh
@@ -4,29 +4,37 @@ cd ~
 
 # Install prerequisites
 
-sudo apt update --yes && \
-sudo apt install --yes build-essential curl default-jdk httpie jq git python-pip python-dev wget && \
-sudo pip install --upgrade pip
-
 # This should be the value of Legacy API Token, found here: https://network.pivotal.io/users/dashboard/edit-profile
 PIVNET_UAA_REFRESH_TOKEN=change_me
 
+# Set versions of installed software
 AZ_REPO=$(lsb_release -cs)
 BOSH_VERSION=6.2.1
 CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
 LEFTOVERS_VERSION=0.62.0
 PIVNET_VERSION=1.0.1
-TF_VERSION=0.12.23
+TF_VERSION=0.12.24
 
-pip install awscli
-
+# Add package sources and repositories
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | \
 sudo tee /etc/apt/sources.list.d/azure-cli.list && \
 sudo apt-key --keyring /etc/apt/trusted.gpg.d/Microsoft.gpg adv \
 --keyserver packages.microsoft.com \
---recv-keys BC528686B50D79E339D3721CEB3E94ADBE1229CF && \
-sudo apt update && \
-sudo apt install --yes azure-cli
+--recv-keys BC528686B50D79E339D3721CEB3E94ADBE1229CF
+
+echo "deb http://packages.cloud.google.com/apt ${CLOUD_SDK_REPO} main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list && \
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add - && \
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys CC86BB64
+
+sudo add-apt-repository ppa:rmescandon/yq -y
+
+# Download package information from all configured sources and install a complement of software
+sudo apt update --yes && \
+sudo apt install --yes azure-cli build-essential curl default-jdk docker.io google-cloud-sdk httpie jq git python-pip python-dev unzip wget yq
+
+
+sudo pip install --upgrade pip
+pip install awscli
 
 wget -O bosh https://s3.amazonaws.com/bosh-cli-artifacts/bosh-cli-${BOSH_VERSION}-linux-amd64 && \
 chmod +x bosh && \
@@ -40,14 +48,9 @@ sudo mv cf7 /usr/local/bin
 
 cf install-plugin -r CF-Community "log-cache" -f
 
-sudo apt install --yes docker.io && \
 sudo systemctl start docker && \
 sudo systemctl enable docker && \
 sudo usermod -aG docker ${USER}
-
-echo "deb http://packages.cloud.google.com/apt ${CLOUD_SDK_REPO} main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list && \
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add - && \
-apt update -y && apt install google-cloud-sdk -y
 
 curl -L https://k14s.io/install.sh | sudo bash
 
@@ -74,8 +77,3 @@ wget https://github.com/genevieve/leftovers/releases/download/v${LEFTOVERS_VERSI
 mv leftovers-v${LEFTOVERS_VERSION}-linux-amd64 leftovers && \
 chmod +x leftovers && \
 sudo mv leftovers /usr/local/bin
-
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys CC86BB64 && \
-sudo add-apt-repository ppa:rmescandon/yq && \
-sudo apt update && \
-sudo apt install yq --yes
